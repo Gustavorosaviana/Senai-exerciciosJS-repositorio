91fe4c4ba5b96f8cb6693d9e6e96ae075b9b497a
Gustavorosaviana <gustavoviana340@gmail.com>
2026-05-21 17:16:52 -0300

    Exercicio da Lista - Usando Funções

--- Funcoes/funcoes.js
+++ Funcoes/funcoes.js
@@ -12,7 +12,7 @@
 
 // console.log(numerosDobrados);
 
-// mesma função, mas agor aomc arrow function
+// mesma função, mas agora com arrow function
 // let numerosDobradosArrow = numeros.map(num => num * 2);
 // console.log(numerosDobradosArrow)
 
@@ -87,3 +87,109 @@
 // console.log(nome.toLowerCase()); // julia
 // console.log(cargo.toUpperCase()); // AMOR DA MINHA VIDA
 
+// SOME -> Verifica se pelo menos um item da lista atende a condição
+// let numeros = [1, 3, 5, 8];
+
+// let temPar = numeros.some(num => num % 2 === 0); // true ou false
+// console.log(temPar) // true
+
+// // EVERY -> Verifica se TODOS os elementos da lista atendem a uma condição
+// let todosPares = numeros.every(num => num % 2 === 0);
+// console.log(todosPares); // false
+
+// SORT -> Ordena todos os elementos de uma lista
+// let numeros = [3, 10, 5, 2, 4]
+// let letras = ["c", "a", "x", "h"]
+
+// // ordenando letras
+// letras.sort();
+// console.log(letras);
+
+// // ordenando números
+// numeros.sort((a, b) => a - b);
+// console.log(numeros);
+
+// // REVERSE -> Inverte a ordem de array
+// numeros.reverse();
+// console.log(numeros);
+
+// JOIN -> junta os elementos de um array em uma string
+// let palavras = ["JS", "é", "muito", "bom!"];
+
+// let frase = palavras.join(" ");
+// console.log(frase);
+
+
+// PUSH -> Adicionar elementos no final do array
+// POP -> Remover elementos no final do array
+// let lista = ["A", "B"];
+
+// lista.push("C"); // adicionado
+// console.log(lista);
+
+// lista.pop(); // removendo
+// console.log(lista);
+
+// SHIFT -> Remover elementos do inicio do array
+// UNSHIFT -> Adicionar elementos no inicio do array
+// let lista = ["B", "C"]
+
+// lista.unshift("A"); // adiciona
+// console.log(lista);
+
+// lista.shift(); // remove
+// console.log(lista);
+
+
+// SLICE -> Cria uma cópia de uma parte da lista
+/*
+    array.slice(inicial, final)
+
+    inicial -> onde começa (a copiar o array)
+    final -> onde para (não inclui a posição final (indíce / index))
+*/
+
+// let numeros = [1, 2, 3, 4]
+// let copia = numeros.slice(1, 3); // 1 e 3 são posições do array
+
+// console.log(copia);
+
+
+// SPLICE -> Remove ou adiciona elementos em qualquer posição
+/*
+    array.splice(indice, count, item1, ..., itemX)
+
+    indice -> posição do elemento no array(index)
+    count -> número de itens a serem removidos (pode ser 0)
+    item -> itens que vão ser adicionados
+*/
+
+// let numeros = [1, 2, 3, 4];
+
+// numeros.splice(1, 1);
+// console.log(numeros);
+
+
+// let frutas = ["Banana", "Laranja", "Maçã", "Manga"]
+
+// frutas.splice(0, 3, "Limão", "Kiwi");
+// console.log(frutas);
+
+// let frutasNovas = ["Banana", "Laranja", "Maçã", "Manga"]
+
+// frutasNovas.splice(2, 0, "Limão", "Kiwi");
+// console.log(frutasNovas);
+
+// REPLACE -> Substitui uma parte da string
+// let texto = "Olá mundo!";
+
+// // replace(valorProcurado, valorSubstituto)
+// let novoTexto = texto.replace("mundo", "cliente");
+// console.log(novoTexto);
+
+
+// SPOILER.........
+// let textoP = document.getElementById("texto");
+// let saudacao = prompt("Digite uma saudação: ");
+
+// textoP.textContent = saudacao == "bom dia" ? saudacao : "QUINTOUUUUUU!!";
