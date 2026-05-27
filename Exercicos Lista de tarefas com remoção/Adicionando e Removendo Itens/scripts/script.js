47963578dc70e694056bebf1311db90d568078be
Gustavorosaviana <gustavoviana340@gmail.com>
2026-05-27 17:23:12 -0300

    Exercicio Lista com remocao

---
+++ Exercicos Lista de tarefas com remoção/Adicionando e Removendo Itens/scripts/script.js
@@ -0,0 +1,34 @@
+const input = document.getElementById("input");
+const botaoAdicionar = document.getElementById("botao-adicionar");
+const lista = document.getElementById("lista");
+const mensagem = document.getElementById("mensagem");
+const botaoRemover = document.getElementById("botao-remover");
+
+botaoAdicionar.addEventListener("click", function () {
+
+    const tarefa = input.value;
+
+    if (tarefa === "") {
+        mensagem.innerText = "Digite uma tarefa!";
+        return;
+    }
+
+    mensagem.innerText = "";
+
+    const item = document.createElement("li");
+
+    item.innerText = tarefa;
+
+    lista.appendChild(item);
+
+    input.value = "";
+
+    item.addEventListener("click", function () {
+        item.remove();
+    });
+
+});
+
+// botaoRemover.addEventListener("click", function () {
+//     lista.remove();
+// })
