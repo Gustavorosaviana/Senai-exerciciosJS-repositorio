89c974c1ce8a9c48c6c442fc32221ce233195bb0
Gustavorosaviana <gustavoviana340@gmail.com>
2026-05-27 17:21:21 -0300

    Exercicio Lista de Itens

---
+++ Armazenamento/scripts/script.js
@@ -0,0 +1,30 @@
+const inputTarefa = document.getElementById("input-tarefa");
+const botaoAdicionar = document.getElementById("botao-adicionar");
+const listaTarefas = document.getElementById("lista-tarefa");
+
+// criando uma lista vazia
+let tarefas = [];
+
+// função para salvar tarefas no local storage
+function salvarTarefas() {
+
+    /*
+        localStorage -> armazenamento local do navegador
+        setItem -> salva no armazenamento o conteúdo recebido
+        JSON.stringify(tarefas) -> pega  a lista de tarefas, converte para texto (string) e armazena esse texto
+    */
+localStorage.setItem("tarefas", JSON.stringify(tarefas));
+}
+
+// função para mostrar tarefas na tela
+function mostrarTarefas() {
+    listaTarefas.innerHTML =""; // limpa a lista de tarefas na tela
+
+    //[tomar cafe, almoçar, jantar]
+    for(let i = 0; i < tarefas.length; i++) {
+        // para cada um elemento, cria um li
+        const li = document.createElement("li");
+        // cada vez que criar o li, passa o valor que está na lista de tarefas na posição do li
+        li.innerText = tarefas[i];
+    }
+}
