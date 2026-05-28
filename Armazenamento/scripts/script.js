6323d1150b51c35ad4d17d8d8957c8ffc3fa2919
Gustavorosaviana <gustavoviana340@gmail.com>
2026-05-28 17:22:11 -0300

    Atualizacao Armazenamento

--- Armazenamento/scripts/script.js
+++ Armazenamento/scripts/script.js
@@ -26,5 +26,69 @@ function mostrarTarefas() {
         const li = document.createElement("li");
         // cada vez que criar o li, passa o valor que está na lista de tarefas na posição do li
         li.innerText = tarefas[i];
+
+        const botaoRemover = document.createElement("button");
+        botaoRemover.innerText = "🗑️";
+        
+        // cria classe para usar estilização do css
+        botaoRemover.className = "botao-remover";
+
+        botaoRemover.addEventListener("click", () => {
+            // CHAMAR FUNÇÃO PARA REMOVER TAREFA
+            removerTarefas(i);
+        })
+
+        li.appendChild(botaoRemover);
+        listaTarefas.appendChild(li);
     }
 }
+
+function removerTarefas(posicaoTarefa) {
+
+    // splice -> (posicaoInicial, qtde_Itens)
+    tarefas.splice(posicaoTarefa, 1);
+
+    // depois de remover, chamo a função de salvar no localSotarge
+    // atualiza o localStorage com array de tarefas atualizado
+    salvarTarefas();
+
+    // mostra as tarefas atualizadas, sem as tarefas que foram removidas
+    mostrarTarefas();
+
+}
+
+function adiconarTarefas() {
+    const valortarefa = inputTarefa.value;
+
+    if(valortarefa.trim() === "") {
+        alert("Digite uma tarefa!");
+        return; // não deixa a tarefa vazia aparecer na tela
+    }
+
+    // adiciona tarefas dentro do array
+    tarefas.push(valortarefa);
+    inputTarefa.value = "";
+
+    salvarTarefas();
+    mostrarTarefas();
+
+
+}
+
+// função para carregar as tarefas salvas no localStorage
+function carregarTarefas() {
+    // pega as tarefas e armazena na varíavel 'tarefasSalvas'
+    const TarefasSalvas = localStorage.getItem("tarefas");
+
+    // se existir alguma coisa dentro de tarefasSalvas
+    // então converte a tarefa e mostra na tela
+    if(TarefasSalvas) {
+        // transforma o texto que está no localStorage em array novamente
+        tarefas = JSON.parse(TarefasSalvas);
+        mostrarTarefas();
+    }
+}
+
+botaoAdicionar.addEventListener("click", adiconarTarefas);
+
+carregarTarefas();
